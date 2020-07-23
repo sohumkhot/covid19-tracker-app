9effcc24af6e9d317a92f0705a66d1b64800d521
Sohum Khot <sohumkhot@gmail.com>
2020-07-24 02:42:32 +0530

    Updates

---
+++ precache-manifest.3e2c250757cae8c2d5bb67aa03c20e68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711063f0995795dabf1ba985edff55d9",
+    "url": "/covid19-tracker-app/index.html"
+  },
+  {
+    "revision": "c4bf4c1623fb623d73ea",
+    "url": "/covid19-tracker-app/static/css/main.8b0961b3.chunk.css"
+  },
+  {
+    "revision": "a6ee7bd63a0b7750175c",
+    "url": "/covid19-tracker-app/static/js/2.c5fa8aa3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker-app/static/js/2.c5fa8aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bf4c1623fb623d73ea",
+    "url": "/covid19-tracker-app/static/js/main.385f93c3.chunk.js"
+  },
+  {
+    "revision": "967bd29442fc09d762cd",
+    "url": "/covid19-tracker-app/static/js/runtime-main.a10db3b6.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker-app/static/media/image.d7265326.png"
+  }
+]);
